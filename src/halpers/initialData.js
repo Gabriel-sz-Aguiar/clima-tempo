0647631e2093fbdda2c7f6e7c43ef203f8a32a74
Gabriel-sz-Aguiar <g.sz.aguiar@gmail.com>
2023-06-15 20:46:32 -0300

    first commit

---
+++ src/halpers/initialData.js
@@ -0,0 +1,17 @@
+const initialData = {
+  location: {
+    name: "-",
+    region: "-",
+    country: "-",
+  },
+  current: {
+    temp_c: 0,
+
+    condition: {
+      text: "-",
+      icon: "//cdn.weatherapi.com/weather/64x64/night/296.png",
+    },
+  },
+};
+
+export default initialData;
---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+const KEY = '17cc76b61de247da9e5194300231506 ';
+
+const fetchData = async (city) => {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  
+  const fetchResponse = await fetch(url);
+
+  const data = await fetchResponse.json();
+
+  return data;
+};
+
+export default fetchData;
